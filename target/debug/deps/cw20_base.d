d7a60d7bfeb92d53f7251b4c2438c9c73d876d7a
Mohsin Laeeque <mohsin.laeeque@arhamsoft.com>
2022-02-01 12:58:58 +0500

    clean

--- target/debug/deps/cw20_base.d
+++ target/debug/deps/cw20_base.d
@@ -1,15 +1,15 @@
-/home/shaibiii/localterra/cw20-base/target/debug/deps/libcw20_base.so: src/lib.rs src/allowances.rs src/contract.rs src/enumerable.rs src/error.rs src/msg.rs src/state.rs
+D:\Projects\Proteus\token\target\debug\deps\cw20_base.dll: src\lib.rs src\allowances.rs src\contract.rs src\enumerable.rs src\error.rs src\msg.rs src\state.rs
 
-/home/shaibiii/localterra/cw20-base/target/debug/deps/libcw20_base.rlib: src/lib.rs src/allowances.rs src/contract.rs src/enumerable.rs src/error.rs src/msg.rs src/state.rs
+D:\Projects\Proteus\token\target\debug\deps\libcw20_base.rlib: src\lib.rs src\allowances.rs src\contract.rs src\enumerable.rs src\error.rs src\msg.rs src\state.rs
 
-/home/shaibiii/localterra/cw20-base/target/debug/deps/cw20_base.d: src/lib.rs src/allowances.rs src/contract.rs src/enumerable.rs src/error.rs src/msg.rs src/state.rs
+D:\Projects\Proteus\token\target\debug\deps\cw20_base.d: src\lib.rs src\allowances.rs src\contract.rs src\enumerable.rs src\error.rs src\msg.rs src\state.rs
 
-src/lib.rs:
-src/allowances.rs:
-src/contract.rs:
-src/enumerable.rs:
-src/error.rs:
-src/msg.rs:
-src/state.rs:
+src\lib.rs:
+src\allowances.rs:
+src\contract.rs:
+src\enumerable.rs:
+src\error.rs:
+src\msg.rs:
+src\state.rs:
 
 # env-dep:CARGO_PKG_VERSION=0.9.0
